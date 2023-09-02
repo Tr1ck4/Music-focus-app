f4c06c617eb74fe0437f2407ed1a4cd84c4e3504
LucidNg <94208867+LucidNg@users.noreply.github.com>
2023-09-02 16:31:18 +0700

    Update button in setting page

--- lib/SettingPage.dart
+++ lib/SettingPage.dart
@@ -24,145 +24,215 @@ class _SettingPageState extends State<SettingPage> {
             ),
           ),
           Padding(
-            padding: EdgeInsets.all(40),
+            padding: const EdgeInsets.all(40),
             child: Column(children: [
-              SizedBox(
+              const SizedBox(
                 height: 30,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Container(
-                    child: Image.asset(
-                      "assets/icon/user.png",
-                      height: 50,
-                      width: 50,
-                      color: Colors.deepPurpleAccent.shade200,
+                  Image.asset(
+                    "assets/icon/user.png",
+                    height: 50,
+                    width: 50,
+                    color: Colors.deepPurpleAccent.shade200,
+                  ),
+                  Center(
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        height: 50,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            color: const Color.fromRGBO(156, 38, 68, 1),
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: const [
+                              BoxShadow(
+                                  color: Color.fromRGBO(197, 26, 120, 1),
+                                  spreadRadius: 1,
+                                  blurRadius: 8,
+                                  offset: Offset(4, 4)),
+                              BoxShadow(
+                                  color: Color.fromRGBO(151, 38, 136, 1),
+                                  spreadRadius: 1,
+                                  blurRadius: 8,
+                                  offset: Offset(-4, -4))
+                            ]),
+                        child: const Center(
+                          child: Text(
+                            'Username',
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                          height: 50,
-                          width: 200,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white.withOpacity(0.3))),
-                      Text(
-                        'Username',
-                        style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.redAccent),
-                      ),
-                    ],
-                  )
+                  // Stack(
+                  //   alignment: Alignment.center,
+                  //   children: [
+                  //     Container(
+                  //         height: 50,
+                  //         width: 200,
+                  //         decoration: BoxDecoration(
+                  //             borderRadius: BorderRadius.circular(10),
+                  //             color: Colors.white.withOpacity(0.3))),
+                  //     Text(
+                  //       'Username',
+                  //       style: TextStyle(
+                  //           fontSize: 24,
+                  //           fontWeight: FontWeight.bold,
+                  //           color: Colors.redAccent),
+                  //     ),
+                  //   ],
+                  // )
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 40,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Container(
-                    child: Image.asset(
-                      "assets/icon/tracks.png",
-                      height: 50,
-                      width: 50,
-                      color: Colors.amberAccent.shade400,
+                  Image.asset(
+                    "assets/icon/tracks.png",
+                    height: 50,
+                    width: 50,
+                    color: Colors.amberAccent.shade400,
+                  ),
+                  Center(
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        height: 50,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            color: const Color.fromRGBO(156, 38, 68, 1),
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: const [
+                              BoxShadow(
+                                  color: Color.fromRGBO(197, 26, 120, 1),
+                                  spreadRadius: 1,
+                                  blurRadius: 8,
+                                  offset: Offset(4, 4)),
+                              BoxShadow(
+                                  color: Color.fromRGBO(151, 38, 136, 1),
+                                  spreadRadius: 1,
+                                  blurRadius: 8,
+                                  offset: Offset(-4, -4))
+                            ]),
+                        child: const Center(
+                          child: Text(
+                            'Liked Tracks',
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                          height: 50,
-                          width: 200,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white.withOpacity(0.3))),
-                      Text(
-                        'Liked Tracks',
-                        style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.redAccent.shade200),
-                      ),
-                    ],
-                  )
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 40,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Container(
-                    child: Image.asset(
-                      "assets/icon/recent.png",
-                      height: 50,
-                      width: 50,
-                      color: Colors.cyanAccent.shade700,
+                  Image.asset(
+                    "assets/icon/recent.png",
+                    height: 50,
+                    width: 50,
+                    color: Colors.cyanAccent.shade700,
+                  ),
+                  Center(
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        height: 50,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            color: const Color.fromRGBO(156, 38, 68, 1),
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: const [
+                              BoxShadow(
+                                  color: Color.fromRGBO(197, 26, 120, 1),
+                                  spreadRadius: 1,
+                                  blurRadius: 8,
+                                  offset: Offset(4, 4)),
+                              BoxShadow(
+                                  color: Color.fromRGBO(151, 38, 136, 1),
+                                  spreadRadius: 1,
+                                  blurRadius: 8,
+                                  offset: Offset(-4, -4))
+                            ]),
+                        child: const Center(
+                          child: Text(
+                            'Recent',
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                          height: 50,
-                          width: 200,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white.withOpacity(0.3))),
-                      Text(
-                        'Recent',
-                        style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.redAccent.shade200),
-                      ),
-                    ],
-                  )
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 40,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Container(
-                    child: Image.asset(
-                      "assets/icon/download.png",
-                      height: 50,
-                      width: 50,
-                      color: Colors.greenAccent.shade400,
+                  Image.asset(
+                    "assets/icon/download.png",
+                    height: 50,
+                    width: 50,
+                    color: Colors.greenAccent.shade400,
+                  ),
+                  Center(
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        height: 50,
+                        width: 200,
+                        decoration: BoxDecoration(
+                            color: const Color.fromRGBO(156, 38, 68, 1),
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: const [
+                              BoxShadow(
+                                  color: Color.fromRGBO(197, 26, 120, 1),
+                                  spreadRadius: 1,
+                                  blurRadius: 8,
+                                  offset: Offset(4, 4)),
+                              BoxShadow(
+                                  color: Color.fromRGBO(151, 38, 136, 1),
+                                  spreadRadius: 1,
+                                  blurRadius: 8,
+                                  offset: Offset(-4, -4))
+                            ]),
+                        child: const Center(
+                          child: Text(
+                            'Import ur song',
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                          height: 50,
-                          width: 200,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white.withOpacity(0.3))),
-                      Text(
-                        'Import ur song',
-                        style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.redAccent.shade200),
-                      ),
-                    ],
-                  )
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 40,
               ),
               ClipRRect(
@@ -173,7 +243,7 @@ class _SettingPageState extends State<SettingPage> {
                   width: 130,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 30,
               ),
               Container(
