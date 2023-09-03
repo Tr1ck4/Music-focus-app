586035c247afb3731decb20c7fdb25fec3aa406a
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2023-09-03 17:51:01 +0700

    Update MeditationPage.dart

--- lib/MeditationPage.dart
+++ lib/MeditationPage.dart
@@ -1,3 +1,4 @@
+import 'Task.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:audioplayers/audioplayers.dart';
@@ -10,15 +11,17 @@ class MeditationPage extends StatefulWidget {
 }
 
 class _MeditationPageState extends State<MeditationPage> {
+  int index = 0;
   Timer? countdownTimer;
-  Duration myDuration = const Duration(days: 5);
-  final _audioPlayer = AudioPlayer();
+  Duration myDuration = const Duration(days: 0);
+  final  _audioPlayer = AudioPlayer();
   bool isPlaying = false;
   Duration duration = Duration.zero;
   Duration position = Duration.zero;
   @override
   void initState() {
     super.initState();
+    setAudio();
     _audioPlayer.onPlayerStateChanged.listen((state) {
       setState(() {
         isPlaying = state== PlayerState.playing;
@@ -32,9 +35,19 @@ class _MeditationPageState extends State<MeditationPage> {
     _audioPlayer.onPositionChanged.listen((newPosition) {
       position = newPosition;
     });
+    _audioPlayer.onPlayerComplete.listen((event) {
+      position = Duration.zero;
+      index++;
+      if (index >= meditation.length) {
+        index = 0;
+      }
+      _audioPlayer.play(AssetSource(meditation[index].asset));
+    });
     startTimer();
   }
-
+  Future<void> setAudio() async{
+    _audioPlayer.setSourceAsset(meditation[index].asset);
+  }
   void startTimer() {
     countdownTimer =
         Timer.periodic(const Duration(seconds: 1), (_) => setCountDown());
@@ -130,7 +143,43 @@ class _MeditationPageState extends State<MeditationPage> {
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       IconButton(focusNode: FocusNode(),onPressed: (){Navigator.pop(context);}, icon: const Icon(Icons.arrow_back_ios_new,size: 30,)),
-                      IconButton(onPressed: (){}, icon: const Icon(Icons.list_rounded,size: 30,)),
+                      IconButton(onPressed: (){
+                        showModalBottomSheet(
+                          context: context, builder: (context) {
+                              return Container(
+                                decoration: const BoxDecoration(
+                                  gradient: LinearGradient(colors: [
+                                    Color.fromRGBO(45, 153, 230, 1),
+                                    Color.fromRGBO(101, 190, 255, 1),
+                                    Color.fromRGBO(126, 235, 222, 1)
+                                  ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+                                ),
+                                height: 200,
+                                child: ListView.builder(
+                                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                                    itemCount: meditation_Task.length,
+                                    itemBuilder: (context, index) => Container(
+                                        decoration: const BoxDecoration(
+                                            border: Border(bottom: BorderSide(color: Colors.white,width: 2))
+                                        ),
+                                        child:ListTile(
+                                          title: Text(meditation_Task[index].task),
+                                          leading: IconButton(
+                                            onPressed: () {
+                                              setState(() {
+                                                meditation_Task[index].like = !meditation_Task[index].like;
+                                                print(meditation_Task[index].like?1:0);
+                                              });
+                                            },
+                                            icon: meditation_Task[index].like?const Icon(Icons.album_outlined):const Icon(Icons.album),
+                                          )
+                                        )
+                                    )
+                                )
+                              );
+                          },
+                        );
+                      }, icon: const Icon(Icons.list_rounded,size: 30,)),
                       IconButton(onPressed: (){}, icon: const Icon(Icons.settings,size: 30,))
                     ],
                   ),
@@ -145,11 +194,8 @@ class _MeditationPageState extends State<MeditationPage> {
                   height: 50,
                   child: Slider(
                     inactiveColor: Colors.black12,
-                    allowedInteraction: SliderInteraction.tapAndSlide,
-                    autofocus: false,
-                    focusNode: FocusNode(),
                     min: 0,
-                    max: duration.inSeconds.toDouble(),
+                    max: duration.inSeconds.toDouble()+1,
                     value: position.inSeconds.toDouble(),
                     onChanged: (value) {
                       setState(() async {
@@ -177,22 +223,51 @@ class _MeditationPageState extends State<MeditationPage> {
             Container(
               alignment: Alignment.bottomCenter,
               padding: EdgeInsets.only(bottom:MediaQuery.of(context).size.height/5),
-              child: IconButton(
-                icon: const Icon(Icons.play_arrow,color: Colors.white),
-                onPressed: () async {
-                  if(isPlaying){
-                    await _audioPlayer.pause();
-                  }else{
-                    await _audioPlayer.play(AssetSource('audio/music.mp3'));
-                  }
-                },
-              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () async {
+                      setState(() {
+                        if(index <0)
+                        {index=meditation.length-1;}else{
+                          index--;
+                        }
+                      });
+                      setAudio();
+                    },
+                    icon: const Icon(Icons.skip_previous,color: Colors.white,),
+                  ),
+                  IconButton(
+                    icon: Icon(!isPlaying?Icons.play_arrow:Icons.pause,color: Colors.white),
+                    onPressed: () async {
+                      if(isPlaying){
+                        await _audioPlayer.pause();
+                      }else{
+                        await _audioPlayer.resume();
+                      }
+                    },
+                  ),
+                  IconButton(
+                    onPressed: () async {
+                      setState(() {
+                        if(index >= meditation.length-1)
+                        {index=0;}else{
+                          index++;
+                        }
+                      });
+                      setAudio();
+                    },
+                      icon: const Icon(Icons.skip_next,color: Colors.white,),
+                  )
+                ],
+              )
             ),
             Padding(
                 padding: EdgeInsets.only(top: MediaQuery.of(context).size.height/3),
                 child: SizedBox(
                   height: MediaQuery.of(context).size.height/4,
-                  child: Playlist(list_name: 'Meditation', playlist: meditation),
+                  child: Playlist(list_name: 'Meditation', playlist: meditation,audioPlayer: _audioPlayer),
                 ),
             ),
           ],
