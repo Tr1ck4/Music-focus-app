f71fa40def3add5247f2bea44d3e36568c7eb578
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2023-09-03 17:15:16 +0700

    Revert "Merge pull request #8 from Tr1ck4/main"
    This reverts commit 10a0b9dfd51c7b35695dcf847511289245fa7e1b, reversing
    changes made to 411dbd8dbd7426489cf60e655e32aa5183bf794b.

--- lib/Song.dart
+++ lib/Song.dart
@@ -1,69 +1,43 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-class Song {
+class Song{
   final String name;
   final String asset;
   Song(this.name, this.asset);
 }
 
-List<Song> meditation = [
-  Song('happy', 'audio/music.mp3'),
-  Song('direct', 'audio/direct.mp3')
-];
-
-List<Playlist> storage = [
-  Playlist(list_name: 'meditation', playlist: meditation)
-];
-
-class Playlist extends StatefulWidget {
+List<Song> meditation =  [Song('Forest sounds','audio/Meditation/forest_sounds.mp3'),Song('Waterfall sounds', 'audio/Meditation/waterfall_sounds.mp3')];
+List<Song> sleep =  [Song('Sleeping City','audio/Sleep/SleepingCity_JayLounge.mp3'),Song('WYS', 'audio/Sleep/WYS_LoneftEase.mp3')];
+List<Song> workout =  [Song('Conquer','audio/Workout/Conquer_Hopex.mp3'),Song('IDidThatDiamond_Ortiz', 'audio/Workout/IDidThatDiamond_Ortiz.mp3')];
+List<Song> study =  [Song('Causes_geek','audio/Study/Causes_geek.mp3'),Song('VoiceOfTheForest_YasumuxNo', 'audio/Study/VoiceOfTheForest_YasumuxNo Spirit.mp3')];
+class Playlist extends StatefulWidget{
+  final AudioPlayer audioPlayer;
   final String list_name;
-  final List<Song> playlist;
-
-  int numSong() {
-    int songs = playlist.length;
-    return songs;
-  }
-
-  const Playlist({super.key, required this.list_name, required this.playlist});
+  final List<Song> playlist ;
+  const Playlist({super.key, required this.list_name, required this.playlist,required this.audioPlayer});
   @override
-  State<Playlist> createState() => _PlaylistState();
+  State<Playlist> createState()=>_PlaylistState();
 }
-
-class _PlaylistState extends State<Playlist> {
+class _PlaylistState extends State<Playlist>{
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        padding: const EdgeInsets.symmetric(horizontal: 40),
-        itemCount: widget.playlist.length,
-        itemBuilder: (context, index) => Container(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          itemCount: widget.playlist.length,
+          itemBuilder: (context, index) => Container(
             decoration: const BoxDecoration(
-                border:
-                    Border(bottom: BorderSide(color: Colors.white, width: 2))),
-            child: ListTile(
-              leading: Image.asset(
-                "assets/icon/album.png",
-                height: 40,
-                width: 40,
-                color: Color.fromARGB(255, 218, 152, 53),
-              ),
-              title: Text(
-                widget.playlist[index].name,
-                style: GoogleFonts.openSans(
-                    fontSize: 18,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(
-                widget.list_name,
-                style: TextStyle(color: Colors.white, fontSize: 12),
-              ),
-              trailing: Image.asset(
-                "assets/icon/notliked.png",
-                height: 20,
-                width: 20,
-                color: Colors.white,
-              ),
-            )));
+              border: Border(bottom: BorderSide(color: Colors.white,width: 2))
+            ),
+            child:ListTile(
+                title: Text(widget.playlist[index].name),
+              onTap: () async{
+                setState(() {
+                  widget.audioPlayer.play(AssetSource(widget.playlist[index].asset));
+                });
+              },
+            )
+          )
+    );
   }
 }
